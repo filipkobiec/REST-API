247377676ce6d30b0aed1e568199ac5549d5ea03
FiPiL99 <fipis14@gmail.com>
2022-04-26 15:19:10 +0200

    Add get all products handler

---
+++ EFDataAccessLibrary/Handlers/GetAllProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using EFDataAccessLibrary.Queries;
+using MediatR;
+
+namespace EFDataAccessLibrary.Handlers
+{
+    public class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductsData _data;
+
+        public GetAllProductsHandler(IProductsData data)
+        {
+            _data = data;
+        }
+        public Task<IEnumerable<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken) => Task.FromResult(_data.GetAllProducts());
+    }
+}
