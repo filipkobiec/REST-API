689337c4c40003c377c4ca1311ef1c5993651b3e
FiPiL99 <fipis14@gmail.com>
2022-04-26 17:59:05 +0200

    Add Delete product command and handler

---
+++ EFDataAccessLibrary/Commands/DeleteProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Commands
+{
+    public record DeleteProductCommand(int id) : IRequest<Product>;
+
+}
---
+++ EFDataAccessLibrary/Handlers/DeleteProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using EFDataAccessLibrary.Commands;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Handlers
+{
+    public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, Product>
+    {
+        private readonly IProductsData _data;
+
+        public DeleteProductHandler(IProductsData data)
+        {
+            _data = data;
+        }
+        public Task<Product> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var deletedProduct = Task.FromResult(_data.DeleteProduct(request.id));
+            _data.SaveChanges();
+            return deletedProduct;
+        }
+    }
+}
