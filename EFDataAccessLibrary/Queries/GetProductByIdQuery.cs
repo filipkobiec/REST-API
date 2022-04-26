3394ef9abc0c54e9d67f5425432b5c04753c242c
FiPiL99 <fipis14@gmail.com>
2022-04-26 15:18:44 +0200

    Add get product by id query

---
+++ EFDataAccessLibrary/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using EFDataAccessLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Queries
+{
+    public record GetProductByIdQuery(int id) : IRequest<Product>;
+}
