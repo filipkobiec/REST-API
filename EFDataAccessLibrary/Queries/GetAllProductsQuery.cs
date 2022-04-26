c14f0d934600bad643e006cc608b475660a93d68
FiPiL99 <fipis14@gmail.com>
2022-04-26 15:18:24 +0200

    add get all products query

---
+++ EFDataAccessLibrary/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Queries
+{
+    public record GetAllProductsQuery : IRequest<IEnumerable<Product>>;
+
+}
