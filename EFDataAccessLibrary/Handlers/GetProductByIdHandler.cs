a9f6424566aa4e690811565f963d34b3af4099d0
FiPiL99 <fipis14@gmail.com>
2022-04-26 15:19:30 +0200

    Add get product by id handler

---
+++ EFDataAccessLibrary/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using EFDataAccessLibrary.Queries;
+using MediatR;
+
+
+namespace EFDataAccessLibrary.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductsData _data;
+
+        public GetProductByIdHandler(IProductsData data)
+        {
+            _data = data;
+        }
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return _data.GetProductById(request.id);
+        }
+    }
+}
