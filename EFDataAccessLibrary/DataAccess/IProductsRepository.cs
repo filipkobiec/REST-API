d20f582179d38d5e5e21446b8ac003fc6902e235
FiPiL99 <fipis14@gmail.com>
2022-04-27 14:30:39 +0200

    Add products repository files

---
+++ EFDataAccessLibrary/DataAccess/IProductsRepository.cs
@@ -0,0 +1,19 @@
+﻿using EFDataAccessLibrary.Models;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+
+        Product GetById(int id);
+
+        Product Insert(Product product);
+
+        Product Update(Product product);
+
+        Product Delete(int id);
+
+        int SaveChanges();
+    }
+}
