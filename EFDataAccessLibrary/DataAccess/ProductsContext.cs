eb521dbc613d24789aee7298d3f0865598601908
FiPiL99 <fipis14@gmail.com>
2022-04-27 14:34:21 +0200

    CreationDate is now a private field and date of creation is handled by server

--- EFDataAccessLibrary/DataAccess/ProductsContext.cs
+++ EFDataAccessLibrary/DataAccess/ProductsContext.cs
@@ -15,10 +15,10 @@ namespace EFDataAccessLibrary.DataAccess
         {
             modelBuilder.Entity<Product>()
                 .HasData(
-                new Product { Id = 1, Name = "Product1", ShortDescription = "Very cool product1", Price = 1, CreationDate = new DateTime(2020, 4, 3, 13, 20, 0) },
-                new Product { Id = 2, Name = "Product2", ShortDescription = "Very cool product2", Price = 2, CreationDate = new DateTime(2020, 5, 3, 11, 30, 33) },
-                new Product { Id = 3, Name = "Product3", ShortDescription = "Very cool product3", Price = 3, CreationDate = new DateTime(2021, 5, 6, 8, 20, 0) },
-                new Product { Id = 4, Name = "Product4", ShortDescription = "Very cool product4", Price = 4, CreationDate = new DateTime(2022, 1, 3, 15, 30, 0) });
+                new Product { Id = 1, Name = "Product1", ShortDescription = "Very cool product1", Price = 1 },
+                new Product { Id = 2, Name = "Product2", ShortDescription = "Very cool product2", Price = 2 },
+                new Product { Id = 3, Name = "Product3", ShortDescription = "Very cool product3", Price = 3 },
+                new Product { Id = 4, Name = "Product4", ShortDescription = "Very cool product4", Price = 4 });
         }
 
         public DbSet<Product> Products { get; set; }
--- EFDataAccessLibrary/Models/Product.cs
+++ EFDataAccessLibrary/Models/Product.cs
@@ -1,12 +1,12 @@
 ﻿
 namespace EFDataAccessLibrary.Models
 {
-    public class Product
+    public class Product 
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public decimal Price { get; set; }
-        public DateTime CreationDate { get; set; }
+        public DateTime CreationDate { get; private set; } = DateTime.Now;
     }
 }
