6b0edb43d4ae72ccc98f8a210a3fe6970f182342
FiPiL99 <fipis14@gmail.com>
2022-04-28 13:53:25 +0200

    Create get products tests

---
+++ UnitTests/Products/Queries/GetProductsTests.cs
@@ -0,0 +1,47 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Handlers;
+using EFDataAccessLibrary.Models;
+using EFDataAccessLibrary.Queries;
+using Moq;
+using Shouldly;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitTests.Mocks;
+using Xunit;
+using System.Linq;
+
+
+namespace UnitTests.Products.Queries
+{
+    public class GetProductsTests
+    {
+        private readonly Mock<IProductsRepository> _mockRepository;
+        public GetProductsTests()
+        {
+            _mockRepository = MockProductsRepository.GetMockProductsRepository();
+        }
+
+        [Fact]
+        public async Task GetAllProductsTest()
+        {
+            var handler = new GetAllProductsHandler(_mockRepository.Object);
+            var result = await handler.Handle(new GetAllProductsQuery(), CancellationToken.None);
+
+            result.ShouldBeAssignableTo<IEnumerable<Product>>();
+
+            result.ToList().Count.ShouldBe(4);
+        }
+
+        [Fact]
+        public async Task GetProductByIdTest()
+        {
+            var handler = new GetProductByIdHandler(_mockRepository.Object);
+            var result = await handler.Handle(new GetProductByIdQuery(1), CancellationToken.None);
+
+            result.ShouldBeOfType<Product>();
+
+            result.Id.ShouldBe(1);
+        }
+    }
+}
