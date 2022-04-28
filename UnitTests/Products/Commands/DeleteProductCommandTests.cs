357b31d2206b4e0189c7df1f76361f9c5ffe0f64
FiPiL99 <fipis14@gmail.com>
2022-04-28 15:18:18 +0200

    Add delete product command test

---
+++ UnitTests/Products/Commands/DeleteProductCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using EFDataAccessLibrary.Commands;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Handlers;
+using EFDataAccessLibrary.Models;
+using EFDataAccessLibrary.Queries;
+using Moq;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitTests.Mocks;
+using Xunit;
+
+namespace UnitTests.Products.Commands
+{
+    public class DeleteProductCommandTests
+    {
+        private readonly Mock<IProductsRepository> _mockRepository;
+        public DeleteProductCommandTests()
+        {
+            _mockRepository = MockProductsRepository.GetMockProductsRepository();
+        }
+
+        [Fact]
+        public async Task DeleteCommandTest()
+        {
+            var deleteProductHandler = new DeleteProductHandler(_mockRepository.Object);
+            var productsHandler = new GetAllProductsHandler(_mockRepository.Object);
+
+            var result = await deleteProductHandler.Handle(new DeleteProductCommand(1), CancellationToken.None);
+            result.ShouldBeOfType<Product>();
+
+            var products = await productsHandler.Handle(new GetAllProductsQuery(), CancellationToken.None);
+            products.ToList().Count.ShouldBe(3);
+
+        }
+    }
+}
